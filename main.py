46026ac7f75c84bc5d9bdf4d3ad3ac2de2c4bb47
SE-SalamAQuran <salamquraan789@gmail.com>
2021-04-05 09:44:08 +0300

    adding dataset

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pytesseract
+import cv2
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract"
-print(pytesseract.image_to_string(r"./h.png"))
-lang = pytesseract.get_languages()
-print(lang)
-ar = lang.__getitem__(1)
-pytesseract.image_to_string(r"./h.PNG", lang=ar)
+print(pytesseract.image_to_string(r"./a.PNG"))
+
+arr = cv2.imread("a.png")
+print(arr)
+with open("Cover-letter.txt", "w") as f:
+    print(pytesseract.image_to_string(r"./cover-letter.PNG"), file=f)
+    
+
