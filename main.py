7fd8a561d50eef1d5784e3258e7f99d26411a511
SE-SalamAQuran <salamquraan789@gmail.com>
2021-04-14 03:09:19 +0300

    Test

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import pytesseract
 import cv2
+
 pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract"
 print(pytesseract.image_to_string(r"./a.PNG"))
 
 arr = cv2.imread("a.png")
 print(arr)
-with open("Cover-letter.txt", "w") as f:
-    print(pytesseract.image_to_string(r"./cover-letter.PNG"), file=f)
-    
-
